1dcba871c9296c5b8bd1f9dd8abefe30b20efea1
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-04 01:02:25 +0100

    server.use(express.json()) was missing in server.js
    this has to be specified BEFORE the routes, otherwise the body will be UNDEFINED

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import listEndpoints from "express-list-endpoints";
 
 const server = express();
 
+server.use(express.json()) //this has to be specified BEFORE the routes, otherwise the body will be UNDEFINED 
+
 // ******************* ENDPOINTS ***************************************************
 // a Router is set of endpoints that share something like a prefix (authorsRouter is going to share "/authors as a prefix")
 server.use("/authors", authorsRouter);
